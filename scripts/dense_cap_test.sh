a824be0b8568cd7d684f7cf440900a6e6b92fe6a
InnerPeace-Wu <innerpeace.wu@outlook.com>
2017-12-10 14:20:19 +0800

    vh test setting

--- scripts/dense_cap_test.sh
+++ scripts/dense_cap_test.sh
@@ -7,10 +7,17 @@
 # --------------------------------------------------------
 
 # TODO: change the test procedure.
+set -x
+set -e
+
 GPU_ID=0
 CKPT="/home/joe/git/densecap/output/Densecap_res50_context/vg_1.2_train"
 TEST_IMDB="vg_1.2_test"
 PT_DIR="dense_cap"
+if [ -d '/valohai/outputs' ]; then
+    CKPT="./output/Densecap_res50_context_all/vg_1.2_train"
+fi
+
 time python ./tools/test_net.py  \
   --ckpt ${CKPT} \
   --imdb ${TEST_IMDB} \
